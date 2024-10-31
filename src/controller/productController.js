db4c2d95b0dca1c93530006a07b5367aeb5712b9
Jean <jeanjyjh@gmail.com>
2024-11-01 00:06:21 +1300

    Implement URL query parameter handling to fetch data

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -19,9 +19,14 @@ export const createProduct = async (req, res, next) => {
   }
 };
 
+//백엔드에서 url 쿼리를 통해 데이터를 가져올 때 사용하는 함수
 export const getProducts = async (req, res, next) => {
   try {
-    const products = await Product.find({});
+    const { page, name } = req.query;
+    const condition = name ? { name: { $regex: name, $options: "i" } } : {};
+    let query = Product.find(condition);
+
+    const products = await query.exec();
     return res.status(200).json({ status: "success", products });
   } catch (error) {
     next(error);
