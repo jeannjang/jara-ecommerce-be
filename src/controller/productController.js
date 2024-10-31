e5726210256cbba6538394345a31bfda542c16b0
Jean <jeanjyjh@gmail.com>
2024-11-01 01:59:09 +1300

    Implement getProducts function with pagination and search

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -22,12 +22,22 @@ export const createProduct = async (req, res, next) => {
 //백엔드에서 url 쿼리를 통해 데이터를 가져올 때 사용하는 함수
 export const getProducts = async (req, res, next) => {
   try {
+    const PAGE_SIZE = 4;
     const { page, name } = req.query;
     const condition = name ? { name: { $regex: name, $options: "i" } } : {};
-    let query = Product.find(condition);
+    let query = Product.find(condition).sort({ createdAt: -1 });
+    let response = { status: "success" };
+
+    if (page) {
+      query = query.skip((page - 1) * PAGE_SIZE).limit(PAGE_SIZE);
+      const totalItemNum = await Product.countDocuments(condition);
+      const totalPageNum = Math.ceil(totalItemNum / PAGE_SIZE);
+      response.totalPageNum = totalPageNum;
+    }
 
     const products = await query.exec();
-    return res.status(200).json({ status: "success", products });
+    response.products = products;
+    return res.status(200).json(response);
   } catch (error) {
     next(error);
   }
